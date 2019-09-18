fdbee58df2d139510e01be59f077616560ba51b9
Alex Cook <swordfish@Alexs-MacBook-Pro.local>
2019-09-18 17:24:08 -0600

    Cleanup

--- lib/fibonacci/even_fib.rb
+++ lib/fibonacci/even_fib.rb
@@ -2,26 +2,22 @@ module Fibonacci
   class EvenFibonacci
     attr_accessor :limit, :num1, :num2, :result
 
-    def initialize(limit = 4_000_000)
-      @limit = limit
-      @num1 = 1
-      @num2 = 1
-      @result = [@num1, @num2]
+    def self.even_number_sum(limit = 4_000_000)
+      fibonacci_sequence(limit).select {|n| n.even? }.sum
     end
 
-    def even_number_sum
-      fibonacci_sequence
-      @result.select {|n| n.even? }.inject {|sum, n| sum + n}
-    end
+    def self.fibonacci_sequence(limit)
+      num1 = 1
+      num2 = 1
+      result = [num1, num2]
 
-    def fibonacci_sequence
-      while @num1 + @num2 < @limit
-        add = @num1 + @num2
-        @result << add
-        @num1 = @num2
-        @num2 = add
+      while num1 + num2 < limit
+        add = num1 + num2
+        result << add
+        num1 = num2
+        num2 = add
       end
-      @result
+      result
     end
   end
 end
--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -3,13 +3,11 @@ require "fibonacci"
 describe Fibonacci::EvenFibonacci do
   describe("#even_number_sum") do
     it "should return the sum of even values based on 4_000_000 default limit" do
-      fib = Fibonacci::EvenFibonacci.new()
-      expect(fib.even_number_sum()).to eql(4613732)
+      expect(Fibonacci::EvenFibonacci.even_number_sum()).to eql(4613732)
     end
 
     it "should return the sum of even values based on limit set" do
-      fib = Fibonacci::EvenFibonacci.new(10)
-      expect(fib.even_number_sum()).to eql(10)
+      expect(Fibonacci::EvenFibonacci.even_number_sum(10)).to eql(10)
     end
   end
 end
